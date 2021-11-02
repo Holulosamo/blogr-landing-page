33836d4f30a82543df827c755ad28251561bd765
Holulosamo <eliasrodriguez000@gmail.com>
2021-11-02 19:02:31 -0300

    finishing mobile design

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const d = document,
+    hamburgerBtn = d.querySelector('.hamburguer-btn'),
+    navMenu = d.querySelector('.nav-menu');
+
+hamburgerBtn.addEventListener('click', () => {
+    hamburgerBtn.classList.toggle('close-icon');
+    navMenu.classList.toggle('open')
+});
